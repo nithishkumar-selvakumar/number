d82919040bc12ead55d411a7106ccb1268735815
nithishkumar-selvakumar <46522768+nithishkumar-selvakumar@users.noreply.github.com>
2019-10-01 23:29:47 +0530

    Create NITHISH_F2_LIVE_NEWS.py

---
+++ NITHISH_F2_LIVE_NEWS.py
@@ -0,0 +1,17 @@
+#live news
+
+import requests
+def NewsFromBBC():
+    main_url="https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=4dbc17e007ab436fb66416009dfb59a8"
+    open_bbc_page=requests.get(main_url).json()
+    article=open_bbc_page["articles"]
+    results = []
+    for ar in article:
+        results.append(ar["title"])
+    for i in range(len(results)):
+        print(i+1,results[i])
+
+
+if __name__ == '__main__':
+    NewsFromBBC()
+    
